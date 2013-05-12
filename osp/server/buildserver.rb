4c7cf8a4ec644cba502526316ccf5294690860ba
cpfaff <ctpfaff@arcor.de>
2013-05-12 18:14:45 +0200

    Improves buildserver
    o Adds latest pdf

--- osp/server/buildserver.rb
+++ osp/server/buildserver.rb
@@ -14,28 +14,12 @@
 require 'rubygems'
 require 'fssm'
 
-cmd = "make"
-
 FSSM.monitor do
-  path 'osp/' do
-    glob '**/*'
-    update {|base, relative| system cmd}
-    delete {|base, relative| system cmd}
-    create {|base, relative| system cmd}
-  end
-
-  path 'usr/' do
-    glob '**/*'
-    update {|base, relative| system cmd}
-    delete {|base, relative| system cmd}
-    create {|base, relative| system cmd}
-  end
-
   path '.' do
-    glob 'open_science_paper.Rnw'
-    update {|base, relative| system cmd}
-    delete {|base, relative| system cmd}
-    create {|base, relative| system cmd}
+    cmd = "make"
+    glob ['*.Rnw','*.sty']
+    update { system cmd }
+    delete { system cmd }
+    create { system cmd }
   end
-
 end
